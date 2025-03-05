2bb1dbff5820f9e72465ef317ebc1f51387e728f
Ropex <grow9818@naver.com>
2025-03-05 16:55:51 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -6,33 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
 app.get("/api/customers",(req,res) => {
-    res.send([
-        {
-            id: 1,
-            image: 'https://picsum.photos/64/64',
-            name: '홍길동',
-            birthday: '990309',
-            gender: '남자',
-            job: '대학생',
-        },
-        {
-            id: 2,
-            image: 'https://picsum.photos/64/64',
-            name: '김성민',
-            birthday: '910102',
-            gender: '남자',
-            job: '프로그래머',
-        },
-        {
-            id: 3,
-            image: 'https://picsum.photos/64/64',
-            name: '박민규',
-            birthday: '941004',
-            gender: '남자',
-            job: '개발자',
-        },
-    ]);
+    connection.query(
+        "SELECT * FROM customer",
+        (err,rows,Field) => {
+            res.json(rows);
+        }
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
